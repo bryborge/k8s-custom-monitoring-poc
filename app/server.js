7d462eea3ca69f907d7cf3e73e51b38443e20850
Bryan Borgeson <sonofborge@protonmail.com>
2020-05-28 17:19:44 -0700

    move app metric logic into modules, comment, and clean up

---
+++ app/server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Express = require('express');
+const Prometheus = require('./utils/prometheus');
+
+const App = Express();
+const PORT = 3000;
+
+/**
+ * Get our Counters.
+ */
+App.use(Prometheus.requestCounters);
+App.use(Prometheus.responseCounters);
+
+/**
+ * Kick off default metrics collection.
+ */
+Prometheus.startCollection();
+
+/**
+ * GET '/'.
+ */
+App.get('/', (req, res, next) => {
+  setTimeout(() => {
+    res.json({ message: 'Hello World!' });
+    next();
+  },
+  Math.round(Math.random() * 200));
+});
+
+/**
+ * GET '/metrics'.
+ */
+Prometheus.addMetricsRoute(App);
+
+/**
+ * Serve the application.
+ */
+App.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ app/utils/prometheus.js
@@ -0,0 +1,56 @@
+const Prometheus = require('prom-client');
+const ResponseTime = require('response-time');
+
+/**
+ *
+ */
+module.exports.startCollection = () => {
+  Prometheus.collectDefaultMetrics();
+};
+
+module.exports.requestCounters = (req, res, next) => {
+  if (req.path !== '/metrics') {
+    numOfRequests.inc({ method: req.method });
+    pathsTaken.inc({ path: req.path });
+  }
+  next();
+}
+
+module.exports.responseCounters = ResponseTime( (req, res, time) => {
+  if (req.url !== '/metrics') {
+    responses.labels(req.method, req.url, res.statusCode).observe(time);
+  }
+});
+
+/**
+ * Add the /metrics endpoint.
+ * @param App
+ */
+module.exports.addMetricsRoute = function (App) {
+  App.get('/metrics', (req, res) => {
+    res.set('Content-Type', Prometheus.register.contentType);
+    res.end(Prometheus.register.metrics());
+  });
+};
+
+/**
+ * Helpers
+ */
+
+const numOfRequests = new Prometheus.Counter({
+  name: 'numOfRequests',
+  help: 'Number of requests made',
+  labelNames: ['method']
+});
+
+const pathsTaken = new Prometheus.Counter({
+  name: 'pathsTaken',
+  help: 'Paths taken in the app',
+  labelNames: ['path']
+});
+
+const responses = new Prometheus.Summary({
+  name: 'responses',
+  help: 'Response time in milliseconds',
+  labelNames: ['method', 'path', 'status']
+});
